f0f2657118b3d73c8404f27aeba582c73bdd0fa2
Zenith <z@zenith.dev>
2021-04-29 12:33:49 -0700

    Adjust imports, fix time comparison logic, add role configuration command

--- unearthedarcanabot.py
+++ unearthedarcanabot.py
@@ -9,6 +9,7 @@ import aurflux
 import discord
 import pendulum
 from aurflux.auth import Record
+from aurflux.command import Response
 
 import TOKENS
 import scraper
@@ -26,7 +27,7 @@ class Scraper(aurflux.FluxCog):
 
       while not self.flux.is_ready():
          await asyncio.sleep(1)
-      self.scraper = ScrapeEventer(parent_router=self.flux.router.host, interval=5)
+      self.scraper = ScrapeEventer(parent_router=self.flux.router.host, interval=60 * 60)
       self.scraper.startup()
 
    def load(self) -> None:
@@ -47,23 +48,17 @@ class Scraper(aurflux.FluxCog):
             color=discord.Color.blue() if article['title'].startswith("Survey") else discord.Color.purple()
          )
          embed.add_field(name="Summary", value=article['summary'] + f"\n\n[{article['link']}]({article['link']})")
-         embed.set_footer(text=f"")
-
-         for guild in self.flux.guilds:
-            async with asyncio.Lock():
+         async with asyncio.Lock():
+            for guild in self.flux.guilds:
                gctx = aurflux.context.ManualGuildCtx(flux=self.flux, guild=guild)
                gcfg = self.flux.CONFIG.of(gctx)
 
                if not gcfg["last_post"] or pendulum.parse(gcfg["last_post"]) < dt:
                   async with self.flux.CONFIG.writeable_conf(gctx) as cfg:
                      cfg["last_post"] = dt.isoformat()
-
-               else:
-                  print("sending!")
                   # noinspection PyTypeChecker
                   c: discord.TextChannel = await self.flux.get_channel_s(gcfg["target"])
-                  await c.send(embed=embed)
-                  await c.send(content=gctx.guild.get_role(819690698696687630).mention)
+                  await c.send(embed=embed, content=gctx.guild.get_role(gcfg["role"]).mention if "role" in gcfg else None)
 
       @self._commandeer(name="target", default_auths=[Record.allow_server_manager()])
       async def __target(ctx: aurflux.ty.GuildCommandCtx, target_id: str):
@@ -77,7 +72,7 @@ class Scraper(aurflux.FluxCog):
          :param target_id:
          :return:
          """
-         if not target_id.strip():
+         if not target_id or not target_id.strip():
             async with self.flux.CONFIG.writeable_conf(ctx.msg_ctx) as cfg:
                del cfg["target"]
             return Response(f"Unset target channel")
@@ -89,6 +84,32 @@ class Scraper(aurflux.FluxCog):
          async with self.flux.CONFIG.writeable_conf(ctx.msg_ctx) as cfg:
             cfg["target"] = target_channel[0]
          return Response(f"Set target channel to {ctx.msg_ctx.guild.get_channel(target_channel[0]).mention}")
+
+      @self._commandeer(name="targetrole", default_auths=[Record.allow_server_manager()])
+      async def __targetrole(ctx: aurflux.ty.GuildCommandCtx, target_id: str):
+         """
+         targetrole <target_role>
+         ==
+         sets
+         ==
+         ==
+         :param ctx:
+         :param target_id:
+         :return:
+         """
+         if not target_id or not target_id.strip():
+            async with self.flux.CONFIG.writeable_conf(ctx.msg_ctx) as cfg:
+               del cfg["role"]
+            return Response(f"Unset target role")
+
+         target_role = aurflux.utils.find_mentions(target_id)
+         if not target_role or not (r := ctx.msg_ctx.guild.get_role(target_role[0])):
+            raise aurflux.CommandError(f"Did not find a valid role in `{target_id}`. Please use an ID or mention the role")
+
+         async with self.flux.CONFIG.writeable_conf(ctx.msg_ctx) as cfg:
+            cfg["role"] = r.id
+
+         return Response(f"Set target role to {r.name}")
 
 
 class UABot(aurcore.AurCore):
